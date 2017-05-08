5124fd31a76a8b25814e65aa7f8a2a9a9bc497e4
minorugod <minorugod@users.noreply.github.com>
2017-05-08 21:16:28 +0800

    Update euler23solution.js
    Change Variable names.

--- Question 23/euler23solution.js
+++ Question 23/euler23solution.js
@@ -2,29 +2,41 @@
 
 QUESTION
 
-A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. 
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
-As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, 
+the smallest number that can be written as the sum of two abundant numbers is 24. 
+
+By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
+However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number 
+that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
 ANSWER
 
-"All integers greater than 28123 can be written as the sum of two abundant numbers" therefore,
+  "All integers greater than 28123 can be written as the sum of two abundant numbers" 
+
+  therefore,
 
   Sum of all positive integers which cannot be written as the sum of two abundant numbers 
-= Sum of all numbers (below 28124) - Total sum of two abundant numbers(below 28124).
+
+  is equal to 
+
+  Sum of all numbers (below 28124) -  Summation of the "sum of two abundant numbers(below 28124)".
 
 */
 
-//total49 = Sum of all numbers (below 28124)
-total49 = 0;
+//sumOfAllBelow28214 = Sum of all numbers (below 28124)
+totalSumOfAllBelow28214 = 0;
 for (i=0; i<28214; i++)
 {
-   total49=total49+i;
+  totalSumOfAllBelow28214 = totalSumOfAllBelow28214 + i;
 }
 
-//Abundant49 = Abundant numbers below 28213 (in Array form)
-var Abundant49 = [];
+//abundantBelow28214 = Abundant numbers below 28214 (in Array form)
+var abundantBelow28214 = [];
 
 for (i=12; i<28124; i++){
   var n=1;
@@ -39,35 +51,36 @@ for (i=12; i<28124; i++){
  }
  
  if(n<total) {
-   Abundant49.push(n);
+   abundantBelow28214.push(n);
  }
 }
 
-//New 50 = Sum of two abundant numbers(below 28124).
-var New50 =new Set();
-var g;
+//sumOfAbundantBelow28214 = Sum of two abundant numbers(below 28124).
+var sumOfAbundantBelow28214 = new Set();
+var counter;
 var sumtotal = 0;
 
-for(j=0; j<Abundant49.length; j++)
+for(j=0; j<abundantBelow28214.length; j++)
 {
-   for(i=j; i<Abundant49.length; i++)
+   for(i=j; i<abundantBelow28214.length; i++)
    {
-        g = Abundant49[i]+Abundant49[j];
+        counter = abundantBelow28214[i]+abundantBelow28214[j];
 		
-		if(g<28214){
-		 New50.add(g);	
+		if(counter<28214){
+		 sumOfAbundantBelow28214.add(counter);	
        }
    }
 }
 
-//totalSet = Sum of New 50.
-var totalSet = 0;
+//  totalAbundant = Sum of sumOfAbundantBelow28214.
+var totalAbundant = 0;
 
-for(let item of New50){
-   totalSet = totalSet + item;
+for(let item of sumOfAbundantBelow28214){
+   totalAbundant = totalAbundant + item;
 }
 
-//Subtract "total49" from "totalSet".
-var sum49 = total49-totalSet;
+//Subtract  "totalSumOfAllBelow28214" from "totalAbundant".
+var answer = totalSumOfAllBelow28214-totalAbundant;
 
-console.log(sum49);
+console.log(answer);
+
