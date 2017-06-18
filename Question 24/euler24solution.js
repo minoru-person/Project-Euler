75297d77cadc0b1457091f34ebac1008dadf38c2
minorugod <minorugod@users.noreply.github.com>
2017-06-18 11:07:02 +0800

    Update euler24solution.js
    Improve coding style

--- Question 24/euler24solution.js
+++ Question 24/euler24solution.js
@@ -1,13 +1,12 @@
-/*From codeacademy tutorial (codeacademy.com)*/
+// From codeacademy tutorial (codeacademy.com)
 function factorial(n) {
-         if(n === 0) {
-		   return 1;
-		 }
+       if(n === 0) {
+	    return 1;
+	}
 		 
-		 //This is it recursion!!
-		 return n * factorial(n-1);
-
-		 }
+	//This is it recursion!!
+	    return n * factorial(n-1);
+}
 
 var value;
 var array = [];
@@ -15,7 +14,6 @@ var counter = 1000000;
 var numberList = [0,1,2,3,4,5,6,7,8,9];
 
 /*
-
 My Solution was derived from this webpage (See Combinatorics)
 http://www.mathblog.dk/project-euler-24-millionth-lexicographic-permutation/
 
@@ -23,10 +21,9 @@ http://www.mathblog.dk/project-euler-24-millionth-lexicographic-permutation/
 The last nine digits can be ordered in 9! = 362880 ways. So the first 362880 permutations start with a 0. 
 The permutations from 362881 to 725760 start with a 1 and then the permutations from 725761 to 1088640 starts with a 2. 
 Based on that it is clear that the millionth permutation is in the third interval, and thus must start with a 2. etc "
-
 */
 
-for(var n=9; -1<n; n--)	{	
+for(var n = 9; -1 < n; n--)	{	
 	value = counter / factorial(n);	 // 1,000,000/362880 = 2.76
 	value = Math.ceil(value);        // 3
 	value = value - 1;               // 2
