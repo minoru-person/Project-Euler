a619273e61cdebd3091877ebef9f367ea9a6b5f9
minorugod <minorugod@users.noreply.github.com>
2017-07-23 17:51:00 +0800

    Update euler24solution.js
    Improve comments

--- Question 24/euler24solution.js
+++ Question 24/euler24solution.js
@@ -21,6 +21,33 @@ http://www.mathblog.dk/project-euler-24-millionth-lexicographic-permutation/
 The last nine digits can be ordered in 9! = 362880 ways. So the first 362880 permutations start with a 0. 
 The permutations from 362881 to 725760 start with a 1 and then the permutations from 725761 to 1088640 starts with a 2. 
 Based on that it is clear that the millionth permutation is in the third interval, and thus must start with a 2. etc "
+
+So the answer starts with a 2.
+
+The last eight digits can be ordered in 8! = 40320 ways. 
+So the first 40320 permutation starts with a 0. 
+The permutation from 40321 to 80640 starts with a 1.
+The permutation from 80641 to 120960 starts with a 3 (becaus 2 is already taken).
+The permutation from 120961 to 241920 starts with a 4.
+The permuation from 241921 to 362880 starts with a 5.
+The permuation from 362881 to 725760 starts with a 6.
+The permuation from 725761 to 1088640 starts with a 7.
+Based on that it is clear that millionth permuation is in the 7th interval, and thus must start with 27.etc "
+
+So the answer starst with a 2 and then 7.
+
+1st term is                        [0,1,2,3,4,5,6,7,8,9]
+2nd term is                        [0,1,2,3,4,5,6,7,9,8]
+...
+362,880 (or 9!) term is            [0,9,8,7,6,5,4,3,2,1]
+...
+725,760 (or 362,880 x 2) term is   [1,9,8,7,6,5,4,3,2,0]
+725,761 term is                    [2,0,1,3,4,5,6,7,8,9]                    
+...
+1,088,640 (or 362,880 x 3) term is [2,9,8,7,6,5,4,3,1,0]
+...
+3,628,800 (or 10!) term is         [9,8,7,6,5,4,3,2,1,0]
+
 */
 
 for(var n = 9; -1 < n; n--)	{	
@@ -38,18 +65,3 @@ for(var n = 9; -1 < n; n--)	{
 }
 
 console.log(array);			 
-
-/* Extra Notes
-1st term is                        [0,1,2,3,4,5,6,7,8,9]
-2nd term is                        [0,1,2,3,4,5,6,7,9,8]
-...
-362,880 (or 9!) term is            [0,9,8,7,6,5,4,3,2,1]
-...
-725,760 (or 362,880 x 2) term is   [1,9,8,7,6,5,4,3,2,0]
-725,761 term is                    [2,0,1,3,4,5,6,7,8,9]                    
-...
-1,088,640 (or 362,880 x 3) term is [2,9,8,7,6,5,4,3,1,0]
-...
-3,628,800 (or 10!) term is         [9,8,7,6,5,4,3,2,1,0]
-
-*/
