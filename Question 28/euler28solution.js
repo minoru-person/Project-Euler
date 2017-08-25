64c1189d484ba573f32f2e0db7fa99a737731208
minoru <minorugod@users.noreply.github.com>
2017-08-25 14:39:18 +0800

    Update euler28solution.js
    code covention

--- Question 28/euler28solution.js
+++ Question 28/euler28solution.js
@@ -1,4 +1,4 @@
-  /*
+/*
   The sum of a 1001 by 1001 spiral comprises of
 
   1+(3 + 5 + 7 + 9)                       +(13 + 17 + 21 + 25)+ ... 
@@ -9,24 +9,25 @@
   It sums up all the outer corner values for each spiral and add to a variable called final total.
 
   The values for each corner can be calculated by subtracting (spiral-1) from each the largest corner value, which is (spiral^2).
-  */
+*/
   
-  let finalTotal = 0;
+let finTotal = 0;
 
-  function fourCornerSum(spiral) {
-     let cornerTotal = 0; // this variable needs to be reset for each spiral, therefore it is contained within the function.
+function fourCornerSum(spiral) {
+   let corTotal = 0; // this variable needs to be reset for each spiral, therefore it is contained within the function.
   
-     for (i = spiral*spiral, counter = 1; counter <= 4; i = i-(spiral-1)) {
-	       cornerTotal = cornerTotal + i;
-	       counter++;			     
-     }				   
-      finalTotal = finalTotal + cornerTotal;  
-  }
+   for(i = spiral*spiral, counter = 1; counter <= 4; i = i - (spiral-1) ) {
+       corTotal = corTotal + i;
+       counter++;			     
+    }
+     
+    finTotal = finTotal + corTotal;  
+}
 
-  // Sum of corners for all spiral starting from 3 by 3, to 1001 by 1001.  	 				      
-  for (j = 3; j <= 1001; j = j+2) {
-        fourCornerSum(j);
-  }
+// Sum of corners for all spiral starting from 3 by 3, to 1001 by 1001.  	 				      
+for(j = 3; j <= 1001; j = j+2) {
+    fourCornerSum(j);
+}
 
-  // The value in the middle of the spiral is 1. Add 1 to the final value. 		    
-  console.log(finalTotal+1);
+// The value in the middle of the spiral is 1. Add 1 to the final value. 		    
+console.log(finTotal+1);
