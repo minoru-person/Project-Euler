a9b96031a2f029e1e46bdd2d6c8a76dd27b7d1ea
minoru <minorugod@users.noreply.github.com>
2017-08-25 15:04:07 +0800

    Update euler24solution.js
    code conventions

--- Question 24/euler24solution.js
+++ Question 24/euler24solution.js
@@ -1,11 +1,41 @@
-// From codeacademy tutorial (codeacademy.com)
-function factorial(n) {
-       if(n === 0) {
-	    return 1;
-	}
+// Using Long Division, "longdivisionPartA" function generates the decimal representation for a given dividend/divider.
+// It stores the decimal representation in an array called "array". 
+// the remainder is tracked in the array "arraydividend".
+// the function terminates when the remainder goes to "0" 
+// OR
+// when the function detects a duplicate remainder in array "arraydividend".
+// decimal representation kept in an array called array
+
+var array = [];
+
+// arraydividend keeps track of the remainder in the long division. "0" is inserted.
+
+var arraydividend = [0];
+    
+// longdivisionPartA is the first line of the long division. Determines the decimal place of long division.
+ 
+/*
+NOTE: dividend corresponds to the number that is divided in each line of the long division. 
+Therefore it changes after step in the long division. 
+NOTE: the divider remains the same throughout the whole function.
+*/
+
+function longdivisionPartA(dividend, divider){
+    while (dividend < divider) {
+        dividend = dividend * 10;
+            array.push(0); 
+    // Set fibonacci number into array. Array is revesed to make counting easier.
+    var f1 = [];
+    var f2 = [];
+
+    // From codeacademy tutorial (codeacademy.com)
+    function factorial(n) {
+        if(n === 0) {
+       	return 1;
+    }
 		 
-	//This is it recursion!!
-	    return n * factorial(n-1);
+    //This is it recursion!!
+    return n * factorial(n-1);
 }
 
 var value;
@@ -50,18 +80,22 @@ So the answer starst with a 2 and then 7.
 
 */
 
-for(var n = 9; -1 < n; n--)	{	
-	value = counter / factorial(n);	 // 1,000,000/362880 = 2.76
-	value = Math.ceil(value);        // 3
-	value = value - 1;               // 2, the millionth permutation starts with a 2.
+for(var n = 9; -1 < n; n--){	
+    value = counter / factorial(n);	 // 1,000,000/362880 = 2.76
+    value = Math.ceil(value);        // 3
+    value = value - 1;               // 2, the millionth permutation starts with a 2.
 
-	array.push(numberList[value]);   // Add 3rd item of numberList to array, 
+    array.push(numberList[value]);   // Add 3rd item of numberList to array, 
 	                                 // i.e. array = [2]
 
-	numberList.splice(value, 1);     // Remove 3rd item from numberList. 
+    numberList.splice(value, 1);     // Remove 3rd item from numberList. 
 	                                 // i.e. numberList = [0,1,3,4,5,6,7,8,9] 
 	
-	counter = counter - (value*factorial(n)); // 274240 = 100000 - (2*362800)   
+    counter = counter - (value*factorial(n)); // 274240 = 100000 - (2*362800)   
 }
 
 console.log(array);			 
+
+
+
+
